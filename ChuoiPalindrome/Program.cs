7722aba6dd598b9597e9388b5d578b76272a27eb
aunguyen2511 <141208418+aunguyen2511@users.noreply.github.com>
2024-03-03 20:20:16 +0700

    Update ChuoiPalindrome

--- ChuoiPalindrome/Program.cs
+++ ChuoiPalindrome/Program.cs
@@ -15,37 +15,61 @@ namespace ChuoiPalindrome
         /// </summary>
         /// <param name="input">string need to check</param>
         /// <returns>true: if input is Palindrome, otherwise false</returns>
-        static bool IsPalindrome(string input)
-        {
-            var length = input.Length;
-            for(int i = 0; i < length / 2; i++)
-            {
-                if (input[i] != input[length - 1 - i])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
+        //static bool IsPalindrome(string input)
+        //{
+        //    var length = input.Length;
+        //    for (int i = 0; i < length / 2; i++)
+        //    {
+        //        if (input[i] != input[length - 1 - i])
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return true;
+        //}
 
-        static bool CheckPalindrome(string input) => input == input.Reverse().ToString();
-
-        static void Main(string[] args)
+        static bool CheckPalindrome(string input) => input.SequenceEqual(input.Reverse());
+        
+        //CÁCH 1
+        static void Main (string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.UTF8;
-            List<string> list = new List<string>();
-            Console.Write("Nhập mảng: ");
+            Console.WriteLine("Nhập chuỗi cần kiểm tra: ");
             string input = Console.ReadLine();
-            list.Add(input);
-            if (IsPalindrome(input))
+
+            bool IsPalindrome = CheckPalindrome(input); 
+            if (IsPalindrome)
             {
-                Console.WriteLine("=> Chuỗi đối xứng.");
+                Console.WriteLine($"Chuỗi {input} là chuỗi Palindrome");
             }
             else
             {
-                Console.WriteLine("=> Chuỗi không đối xứng.");
+                Console.WriteLine($"Chuỗi {input} không phải là chuỗi Palindrome");
             }
+
         }
+        
+
+
+        //CÁCH 2
+        //static void Main(string[] args)
+        //{
+        //    Console.OutputEncoding = Encoding.UTF8;
+        //    Console.InputEncoding = Encoding.UTF8;
+        //    List<string> list = new List<string>();
+        //    Console.Write("Nhập mảng: ");
+        //    string input = Console.ReadLine();
+        //    list.Add(input);
+
+        //    if (IsPalindrome(input))
+        //    {
+        //        Console.WriteLine("=> Chuỗi đối xứng.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("=> Chuỗi không đối xứng.");
+        //    }
+        //}
     }
 }
