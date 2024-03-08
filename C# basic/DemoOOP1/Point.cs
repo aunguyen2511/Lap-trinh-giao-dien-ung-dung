b73c3702afe9f5bef09580a1c277930e37f3adcc
aunguyen2511 <141208418+aunguyen2511@users.noreply.github.com>
2024-03-08 21:35:55 +0700

    C# basic

---
+++ C# basic/DemoOOP1/Point.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOP1
+{
+    public class Diem
+    {
+        public double HoanhDo { get; set; }
+        public double TungDo { get; set; }
+        //public Diem() { HoanhDo = 0; TungDo = 0; }
+
+        // hoặc
+
+
+        /// <summary>
+        /// Hàm tạo lớp Điểm
+        /// </summary>
+        /// <param name="xx">tung độ</param>
+        /// <param name="yy">hoành độ</param>
+        public Diem(int xx = 0, int yy = 0) 
+        {  
+            this.HoanhDo = xx; TungDo = yy; 
+        }
+        public void Xuat()
+        {
+            Console.WriteLine($"({this.HoanhDo}, {this.TungDo})");
+        }
+        public override string ToString()
+        {
+            return $"({this.HoanhDo}, {this.TungDo})";
+        }
+    }
+
+    
+}
